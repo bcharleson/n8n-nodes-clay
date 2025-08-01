1505db2689cf885a9aeb40a3ea2870d18d39b35a
Brandon Charleson <brandoncharleson@Mac.attlocal.net>
2025-08-01 10:40:46 -0700

    Fix: Resolve class export issue for n8n installation
    - Fixed index.js export structure to properly expose classes
    - Updated main entry point to dist/index.js
    - Version bump to 0.1.1 for installation fix

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-
-// Export the Clay node and credentials
-const ClayApi_node_1 = require("./nodes/ClayApi/ClayApi.node");
-const ClayApi_credentials_1 = require("./credentials/ClayApi.credentials");
-
 module.exports = {
-    ClayApi: ClayApi_node_1.ClayApi,
-    ClayApiCredentials: ClayApi_credentials_1.ClayApi
+    ...require('./nodes/ClayApi/ClayApi.node'),
+    ...require('./credentials/ClayApi.credentials')
 };
